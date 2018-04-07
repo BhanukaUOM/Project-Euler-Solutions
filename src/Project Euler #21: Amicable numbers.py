ec34a3b1afe31a131b4ce763b920873d0149fa3b
Bhanuka Rathnayaka <bhanuka2017@gmail.com>
2018-04-07 23:36:34 +0530

    Update Project Euler #21: Amicable numbers.py

--- src/Project Euler #21: Amicable numbers.py
+++ src/Project Euler #21: Amicable numbers.py
@@ -3,14 +3,16 @@ import itertools
 
 n = 100001
 
-div_sum = [0 for i in range(n)]
+div_sum = [1 for i in range(n)]
+div_sum[0] = 0
+div_sum[1] = 0
 
 for i in range(2, n):
     sq = int(math.sqrt(i))
-    for j in range(1, sq+1):        
+    for j in range(2, sq+1):        
         if i%j==0:
             p = i//j
-            if p!=i:
+            if p!=j:
                 div_sum[i] += j + p
             else:
                 div_sum[i] += j
